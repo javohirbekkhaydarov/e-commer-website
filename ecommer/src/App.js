303ac4edd39ef24bfb6733715aeefa4aeead8720
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-08-25 23:54:43 +0500

    added router and components

--- ecommer/src/App.js
+++ ecommer/src/App.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+  AuthWrapper,
+} from "./pages";
+
 import "./App.css";
-import Main from "./components/Main";
-function App() {
+const App = () => {
   return (
-    <div>
-      <Main />
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </Fragment>
+    </Router>
   );
-}
+};
 
 export default App;
