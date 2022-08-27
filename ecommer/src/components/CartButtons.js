8b636579a3d30fd8aa485163bae654c52a79eff7
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-08-27 08:21:01 +0500

    lesson38

---
+++ ecommer/src/components/CartButtons.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { FaShoppingCart, FaUserMinus, FaUserPlus } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+const CartButtons = () => {
+  return <div>CartButtons</div>;
+};
+
+export default CartButtons;
