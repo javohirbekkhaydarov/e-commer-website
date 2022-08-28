6222c1115f42b62e79ba2946d0d4ffbddb445af0
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-08-28 14:09:16 +0500

    bug fixed lesson38

--- ecommer/src/context/products_context.js
+++ ecommer/src/context/products_context.js
@@ -1,31 +1,34 @@
-import React, { useContext, useEffect, useState, useReducer } from "react";
-import reducer from "../reducers/product_reducer";
-import { SIDEBAR_OPEN, SIDEBAR_CLOSE } from "../actions";
+import React, {useContext, useEffect, useReducer, useState} from 'react';
+import reducer from '../reducers/product_reducer';
+import {
+    SIDEBAR_OPEN,
+    SIDEBAR_CLOSE
+} from '../actions'
 
 const initialState = {
-  isSidebarOpen: false,
-};
+    isSidebarOpen: false, 
+} 
 
-const ProductContext = React.createContext();
+const ProductsContext = React.createContext();
 
-export const ProductProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const openSidebar = () => {
-    dispatch({ type: SIDEBAR_OPEN });
-  };
-
-  const closeSidebar = () => {
-    dispatch({ type: SIDEBAR_CLOSE });
-  };
-
-  return (
-    <ProductContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
-      {children}
-    </ProductContext.Provider>
-  );
-};
+export const ProductsProvider = ({children}) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    
+    const openSidebar = () => {
+        dispatch({type: SIDEBAR_OPEN})
+    }
+    
+    const closeSidebar = () => {
+        dispatch({type:SIDEBAR_CLOSE})
+    } 
+    
+    return (
+         <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar}}>
+            {children}
+         </ProductsContext.Provider>
+    )
+} 
 
 export const useProductsContext = () => {
-  return useContext(ProductContext);
-};
+    return useContext(ProductsContext)
+  }
