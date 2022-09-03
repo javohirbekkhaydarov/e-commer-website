33fdf1c681464bb78c8b712d8066b1c1872dabc0
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-09-03 23:59:53 +0500

    for fix

--- ecommer/src/context/products_context.js
+++ ecommer/src/context/products_context.js
@@ -1,34 +1,50 @@
-import React, {useContext, useEffect, useReducer, useState} from 'react';
-import reducer from '../reducers/product_reducer';
+import axios from"axios"
+import React, { useContext, useEffect, useReducer, useState } from "react";
+import reducer from "../reducers/product_reducer";
 import {
-    SIDEBAR_OPEN,
-    SIDEBAR_CLOSE
-} from '../actions'
+  SIDEBAR_OPEN,
+  SIDEBAR_CLOSE,
+  GET_PRODUCTS_BEGIN,
+  GET_PRODUCTS_SUCCESS,
+} from "../actions";
 
 const initialState = {
-    isSidebarOpen: false, 
-} 
+  isSidebarOpen: false,
+  products_loading: false,
+  product_error: false,
+  products: [],
+  featured_products: [],
+};
 
 const ProductsContext = React.createContext();
 
-export const ProductsProvider = ({children}) => {
-    const [state, dispatch] = useReducer(reducer, initialState);
-    
-    const openSidebar = () => {
-        dispatch({type: SIDEBAR_OPEN})
+export const ProductsProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const openSidebar = () => {
+    dispatch({ type: SIDEBAR_OPEN });
+  };
+
+  const closeSidebar = () => {
+    dispatch({ type: SIDEBAR_CLOSE });
+  };
+
+  const fetchProducts = async (url) => {
+    dispatch({type: GET_PRODUCTS_BEGIN})
+    try{
+        const response = await
+    }catch{
+        console.log('error');
     }
-    
-    const closeSidebar = () => {
-        dispatch({type:SIDEBAR_CLOSE})
-    } 
-    
-    return (
-         <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar}}>
-            {children}
-         </ProductsContext.Provider>
-    )
-} 
+  }
+
+  return (
+    <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
 
 export const useProductsContext = () => {
-    return useContext(ProductsContext)
-  }
+  return useContext(ProductsContext);
+};
--- ecommer/src/reducers/product_reducer.js
+++ ecommer/src/reducers/product_reducer.js
@@ -16,9 +16,45 @@ const product_reducer = (state, action) => {
   if (action.type === SIDEBAR_CLOSE) {
     return { ...state, isSidebarOpen: false };
   }
-  // if(action.type === ) {
+  if (action.type === GET_PRODUCTS_BEGIN) {
+    return { ...state, products_loading: true };
+  }
 
-  // }
+  if (action.type === GET_PRODUCTS_SUCCESS) {
+    const featured_products = action.payload.filter(
+      (product) => product.featured_products === true
+    );
+    return {
+      ...state,
+      products_loading: false,
+      product: action.payload,
+      featured_products,
+    };
+  }
+  if (action.type === GET_PRODUCTS_ERROR) {
+    return { ...state, products_loading: false, product_error: true };
+  }
+  if (action.type === GET_SINGLE_PRODUCT_BEGIN) {
+    return {
+      ...state,
+      single_product_loading: true,
+      single_product_error: false,
+    };
+  }
+  if (action.type === GET_SINGLE_PRODUCT_SUCCESS) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product: action.payload,
+    };
+  }
+  if (action.type === GET_PRODUCTS_ERROR) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product_error: true,
+    };
+  }
   throw new Error(`No matching ${action.type} = action type`);
 };
 
