d6f1962db43a10a163878789230c3716bc53910f
Javohirbek <javohirbekkhaydarov@gmail.com>
2022-09-24 23:09:18 +0500

    lesson45

--- ecommer/src/context/filter_context.js
+++ ecommer/src/context/filter_context.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useEffect, useContext, useReducer } from "react";
+import reducer from "../reducers/filter_reducer";
 
-const filter_context = () => {
-  return (
-    <div>filter_context</div>
-  )
-}
+import {
+  LOAD_PRODUCTS,
+  SET_GRIDVIEW,
+  SET_LISTVIEW,
+  UPDATE_SORT,
+  SORT_PRODUCTS,
+  UPDATE_FILTERS,
+  FILTER_PRODUCTS,
+  CLEAR_FILTERS,
+} from "../actions";
 
-export default filter_context
+import { useProductsContext } from "./products_context";
+
+const initialState = {
+  filtered_products: [],
+  all_products: [],
+  grid_view: true,
+  sort: "price-lowest",
+  filters: {
+    text: "",
+    company: "all",
+    category: "all",
+    color: "all",
+    min_price: 0,
+    max_price: 0,
+    price: 0,
+    shipping: false,
+  },
+};
+const FilterContext = React.createContext();
+
+export const FilteredProvider = ({ children }) => {
+  const { products } = useProductsContext();
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return <FilterContext.Provider  value={{}}>{children}</FilterContext.Provider>;
+};
+
+export const useFilterContext = () => {
+  return useContext(FilterContext);
+};
